b18e0c06e36a76a08cc321a23ded8dc7495ac5d0
Llydia Cross <llydiacross@gmail.com>
2023-01-05 00:34:35 +0000

    fixes

--- scripts/example.ts
+++ scripts/example.ts
@@ -1,11 +1,11 @@
-import type {InfinityMintScript} from 'infinitymint/dist/app/interfaces';
+import type { InfinityMintScript } from "@app/interfaces";
 
 const script: InfinityMintScript = {
-	name: 'My Example Scriptt',
-	description:
-    'This is an example of how you can create a custom InfinityMint script',
-	async execute(IM) {
-		IM.log('Hello World');
-	},
+  name: "My Example Script",
+  description:
+    "This is an example of how you can create a custom InfinityMint script",
+  async execute(IM) {
+    IM.log("Hello World");
+  },
 };
 export default script;
